f064699857ef9059b5337208da162ff7507f629b
trohi <dzenan_95@hotmail.com>
2020-08-05 14:00:04 +0200

    Set up inital project layout through Angular CLI.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component'
+import { CardComponent } from './card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:CardComponent},
+  {path:'signup', component:SignupComponent},
+  {path:'signin', component:SigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
